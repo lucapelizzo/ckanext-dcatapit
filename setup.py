fd53d283a698d62ff9f4dc8bd4b15ddccf8f7205
Alessio Cimarelli <jenkin@dataninja.it>
2019-09-03 15:39:29 +0200

    Fix README.

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     long_description=long_description,
 
     # The project's main homepage.
-    url='https://github.com/geosolutions/ckanext-dcatapit',
+    url='https://github.com/italia/ckanext-dcatapit',
 
     # Author details
     author='''Tobia Di Pisa''',
